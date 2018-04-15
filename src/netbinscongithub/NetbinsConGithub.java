ca211a60d6a38c7e1397cddced39fa21dd2d1972
Dell <Dell@192.168.0.13>
2018-04-15 12:57:44 -0600

    Para salir bien mañana

---
+++ src/netbinscongithub/NetbinsConGithub.java
@@ -0,0 +1,19 @@
+package netbinscongithub;
+
+/**
+ * fecha: 15/04/2018
+ * version: 1.0
+ * copyright: Andrés Suncín
+ * @author Andrés Suncín
+ */
+public class NetbinsConGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double porQueVectores[] = new double[10];
+       String queCuel[] = new String[60];
+    }
+    
+}
